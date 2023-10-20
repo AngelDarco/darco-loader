a8a297e1e0316efe8ee7d08c2aa6d69a4a45b7cb
Angel Darco <angel92-eme@hotmail.com>
2023-10-20 00:47:47 -0500

    :hammer: vite config to build component

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,38 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import dts from "vite-plugin-dts";
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build:{
     lib:{
-      name: "Loader",
+      name: "index",
+      fileName: "index",
       entry: "./src/components/Loader.tsx",
-      formats: ['es', 'cjs', 'umd']
+      formats: ["es", "umd", "cjs"],
+    },
+    minify: true,
+    emptyOutDir: true,
+
+    rollupOptions:{
+      external: ["react", "react-dom"],
+      output:{
+        dir: "dist",
+        exports: "named",
+        globals:{
+          react: "React", "react-dom": "ReactDOM"
+        }
+      }
     }
   },
   plugins: [
+    react(),
     cssInjectedByJsPlugin(),
-    react()],
-})
+    dts({ 
+      rollupTypes: true,
+      outDir: "dist/@types/",
+    })
+  ],
+});
